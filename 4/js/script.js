cd112fe2f829796a8e12003d0f264a646f91d819
xexxy <kratiukartiom@gmail.com>
2019-05-15 12:38:43 +0300

    add code

---
+++ 4/js/script.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+console.log(acc);
