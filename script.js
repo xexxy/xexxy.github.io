15586a62173368dc67edc13805618bec3e17f761
xexxy <38164075+xexxy@users.noreply.github.com>
2020-09-19 00:20:55 +0300

    Put things in order

---
+++ script.js
@@ -0,0 +1,68 @@
+// 'use strict';
+
+// let ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+// ask(
+// 	"Вы согласны?",
+// 	() => alert("Вы согласились."),
+//   	() => alert("Вы отменили выполнение.")
+// );
+
+// let a = 1;
+// let b = a++ + 1;
+
+// alert(b);
+// alert(a);
+
+// function isEmpty(obj) {
+// 	for (let key in obj) {
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+
+// let salaries = {
+// 	John: 100,
+// 	Ann: 160,
+// 	Pete: 130
+// }
+
+// function sumSalary(obj) {
+// 	let sum = 0;
+// 	for (let key in obj) {
+// 		sum += obj[key];
+// 	}
+// 	return sum;
+// }
+
+// function multiplyMumeric(obj) {
+// 	for (let key in obj) {
+// 		if (typeof obj[key] == "number") {
+// 			obj[key] *= 2;
+// 		}
+// 	}
+// }
+
+
+// let calculator = {
+// 	read() {
+// 		this.userNumber1 = prompt('Ввежите 1', '');
+// 		this.userNumber2 = prompt('Ввежите 2', '');
+// 	},
+// 	sum() {
+// 		return this.userNumber1 + this.userNumber2;
+// 	},
+// 	mul() {
+// 		return this.userNumber1 * this.userNumber2;
+// 	}
+// }
+
+// calculator.read();
+
+// alert( calculator.sum() );
+// alert( calculator.mul() );
+
+
+
+
